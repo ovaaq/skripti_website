c6117ee9e31119fc553a40b9ab467201f19ef8e4
SimoTu <50102833+SimoTu@users.noreply.github.com>
2019-11-18 16:43:14 +0200

    Testi update

---
+++ back/api/routes/tuutorit.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const routes = express.Router();
+
+const mysql = require('mysql'); 
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Makkaraperunat1",
+    database: "db"
+    });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Tuutorit is connected");
+    });
+
+
+
+// GET all tuutorit from the DB
+routes.get('/', (req, res, next) => {
+    let sql = "SELECT tuutorit.tuutorit_id,tuutorit.start_time,persons.firstname,persons.lastname, persons.info FROM TUUTORIT INNER JOIN PERSONS ON TUUTORIT.person_id = PERSONS.person_id;";
+    
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.status(200).json({
+            message: 'Tuutorit were fetched',
+            list: result
+        });
+   });
+});
+
+// GET one tuutori with matching if drom DB
+routes.get('/:id', (req, res, next) => {
+    var id = req.params.id;
+    let sql = 'SELECT tuutorit.tuutorit_id,tuutorit.start_time,persons.firstname,persons.lastname, persons.info FROM TUUTORIT INNER JOIN PERSONS ON TUUTORIT.person_id = PERSONS.person_id WHERE tuutorit_id =' + id;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.status(200).json({
+            message: 'Tuutori was searched',
+            user: result
+        });
+   });
+});
+
+// POST one tuutori with JSON and ADD to the DB
+routes.post('/:person_id/:start_time', (req, res, next) => {
+    const tuutori = {
+        person_id: req.params.person_id,
+        start_time: req.params.start_time
+        
+    
+        };
+    let sql = 'INSERT INTO TUUTORIT (person_id, start_time) VALUES('+tuutori.person_id+',"'+tuutori.start_time+'");';
+        con.query(sql, (err, result) => {
+         if(err) throw err;
+         console.log(result);
+         res.status(201).json({
+            message: 'Tuutori was created',
+            object: tuutori   
+        });
+    });
+});
+
+// DELETE one tuutori with matching id and clear in DB
+routes.delete('/:id', (req, res, next) => {
+    var id = req.params.id;
+    let sql = 'DELETE from TUUTORIT WHERE tuutorit_id =' + id;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.status(201).json({
+            message: 'Tuutori was deleted'
+        });
+    });
+});
+
+// PUT new info on tuutori with JSON and change in the DB
+routes.put('/:id/:person_id/:start_time', (req, res, next) => {
+    
+    const tuutori = {
+        id: req.params.id,
+        person_id: req.params.person_id,
+        start_time: req.params.start_time
+        };
+    let sql = 'UPDATE TUUTORIT SET person_id = '+tuutori.person_id+', start_time = "'+tuutori.start_time+'" WHERE tuutorit_id ='+tuutori.id+';';
+        con.query(sql, (err, result) => {
+         if(err) throw err;
+         console.log(result);
+         res.status(201).json({
+            message: 'Tuutori was updated',
+            object: tuutori   
+        });
+    });
+});
+
+
+module.exports = routes;
--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-const port = 8000;
-
-const server = http.createServer(app);
-
-server.listen(port);
-console.log("Server started on port "+ port);
